90fc52cae37fd44f57c4d5bba0574fb032241302
DctrG <jg.serrano@me.com>
2019-07-04 16:09:27 +0800

    Add main function and re-organize code structure

--- realipfinder.py
+++ realipfinder.py
@@ -1,10 +1,6 @@
-from os import getenv
-from dotenv import load_dotenv, find_dotenv
 import argparse
-import ssl
 import sys
 from datetime import datetime, timedelta
-import os
 from pathlib import Path
 import xml.etree.ElementTree as ET
 import re
@@ -14,33 +10,29 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from functools import reduce
 import operator
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-import urllib.request
 
-load_dotenv(find_dotenv())
 
-parser = argparse.ArgumentParser()
-parser.add_argument("firewall_ip", nargs="?", default="192.168.1.120", help="Optional. If nothing entered '192.168.1.120' will be used")
-parser.add_argument("username", nargs="?", default="admin", help="Optional. If nothing entered 'admin' will be used")
-parser.add_argument("password", nargs="?", default="admin", help="Optional. If nothing entered 'admin' will be used")
-args = parser.parse_args()
-
-user = args.username
-password = args.password
-fw_ip = args.firewall_ip
-
-api_url = f"https://{fw_ip}/api/?type=keygen&user={user}&password={password}"
-get_api = requests.get(api_url, verify=False).content
-parsed = ET.fromstring(get_api)
-api_key = parsed[0][0].text
-
-base_url = f'https://{fw_ip}/api/?&key={api_key}'
 pcaps_path = 'test-pcaps/'
-time_range = datetime.now() - timedelta(days = 1)
+# time_range = datetime.now() - timedelta(hours = period)
+time_range = datetime.now() - timedelta(hours = 23)
 threat_time = time_range.strftime('%Y/%m/%d %H:%M:%S')
-now = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+NOW = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
 FIELDS = ['Cdn-Src-Ip','X-Forwarded-For','RealIp','X-Ser','Client-Ip']
 
-def download_pcaps(pcap_id, pcap_time):
+def get_api_key():
+    try:
+        api_url = f"https://{fw_ip}/api/?type=keygen&user={user}&password={password}"
+        # api_url = f"https://1.1.1.1/api/?type=keygen&user={user}&password={password}"
+
+        get_api = requests.get(api_url, verify=False).content
+        parsed = ET.fromstring(get_api)
+        api_key = parsed[0][0].text
+        return api_key
+
+    except requests.exceptions.RequestException as e:
+        return e
+
+def download_pcaps(base_url, pcap_id, pcap_time):
     try:
         url = f'{base_url}&type=export&category=threat-pcap&pcap-id={pcap_id}&search-time={pcap_time}'
         res = requests.get(url, verify=False)
@@ -55,34 +47,37 @@ def download_pcaps(pcap_id, pcap_time):
     except requests.exceptions.RequestException as e:
         return e
 
-def get_threat_logs():
-    url = f"{base_url}&type=log&log-type=threat&query=(receive_time geq '{threat_time}')"
-    res = requests.get(url, verify=False)
-    root = ET.fromstring(res.content)
-    job_id = root[0][1].text
-    job_url = f'{base_url}&type=log&action=get&job-id={job_id}'
-    res_job = requests.get(job_url, verify=False)
-    logroot = ET.fromstring(res_job.content)
-    pcaps_found = 0
-    for child in logroot.findall('.//entry'):
-        if child.tag != 'name':
-            if child.find('flag-pcap') is not None:
-                pcaps_found += 1
-                if pcaps_found == 1:
-                    print('*' * 55)
-                    print('  Found some pcaps in threat logs, downloading...')
-                    print('*' * 55)
-                    import time
-                if child.find('flag-pcap').text == 'yes':
-                    if child.find('pcap_id') is not None:
-                        pcapid = child.find('pcap_id').text
-                    if child.find('receive_time') is not None:
-                        pcaptime = child.find('receive_time').text
-                    download_pcaps(pcapid, pcaptime)
-                    sys.stdout.write(f'\r{pcaps_found} pcaps downloaded ')
-                    sys.stdout.flush()
-    return pcaps_found
-
+def get_threat_logs(base_url):
+    try:
+        url = f"{base_url}&type=log&log-type=threat&query=(receive_time geq '{threat_time}')"
+        res = requests.get(url, verify=False)
+        root = ET.fromstring(res.content)
+        job_id = root[0][1].text
+        job_url = f'{base_url}&type=log&action=get&job-id={job_id}'
+        res_job = requests.get(job_url, verify=False)
+        logroot = ET.fromstring(res_job.content)
+        pcaps_found = 0
+        for child in logroot.findall('.//entry'):
+            if child.tag != 'name':
+                if child.find('flag-pcap') is not None:
+                    pcaps_found += 1
+                    if pcaps_found == 1:
+                        print('*' * 55)
+                        print('  Found some pcaps in threat logs, downloading...')
+                        print('*' * 55)
+                        import time
+                    if child.find('flag-pcap').text == 'yes':
+                        if child.find('pcap_id') is not None:
+                            pcapid = child.find('pcap_id').text
+                        if child.find('receive_time') is not None:
+                            pcaptime = child.find('receive_time').text
+                        download_pcaps(base_url, pcapid, pcaptime)
+                        sys.stdout.write(f'\r{pcaps_found} pcaps downloaded ')
+                        sys.stdout.flush()
+        return pcaps_found
+    except requests.exceptions.RequestException as e:
+        return e
+        
 def find_real_src_ip(pcap):
     cap = pyshark.FileCapture(pcap)
     text = str(cap[0])
@@ -111,18 +106,36 @@ def find_pcap_files():
         return results
     return False
         
-pcaps_in_logs = get_threat_logs()
-if pcaps_in_logs > 0:
-    print(f'from in Threat Logs between {threat_time} and {now}')
-    print(f"Searching for possible hidden original attackers IP's...")
-    indicators_in_pcaps = find_pcap_files()
-    final_result = set(reduce(operator.concat, indicators_in_pcaps))
-    if len(indicators_in_pcaps) > 0:
-        print(f'Found real source ip addresses from offenders: ')
-        print(', '.join(str(x) for x in final_result))
+
+def main(args):
+    api_key = get_api_key()
+    base_url = f'https://{fw_ip}/api/?&key={api_key}'
+    pcaps_in_logs = get_threat_logs(base_url)
+    if pcaps_in_logs > 0:
+        print(f'from in Threat Logs between {threat_time} and {NOW}')
+        print(f"Searching for possible hidden original attackers IP's...")
+        indicators_in_pcaps = find_pcap_files()
+        final_result = set(reduce(operator.concat, indicators_in_pcaps))
+        if len(indicators_in_pcaps) > 0:
+            print(f'Found real source ip addresses from offenders: ')
+            print(', '.join(str(x) for x in final_result))
+        else:
+            print('No indicators found in the pcaps')
     else:
-        print('No indicators found in the pcaps')
-else:
-    print('No pcaps found in threat logs')
+        print('No pcaps found in threat logs')
 
+if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("firewall_ip", nargs="?", default="192.168.1.120", help="Optional. If nothing entered '192.168.1.120' will be used")
+    parser.add_argument("username", nargs="?", default="admin", help="Optional. If nothing entered 'admin' will be used")
+    parser.add_argument("password", nargs="?", default="admin", help="Optional. If nothing entered 'admin' will be used")
+    parser.add_argument("hours", nargs="?", default=1, help="Optional. Timeframe in hours. If nothing entered the last hour will be used")
+    args = parser.parse_args()
+
+    user = args.username
+    password = args.password
+    fw_ip = args.firewall_ip
+    period = args.hours
+    
+    main(args)
